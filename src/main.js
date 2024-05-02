8268bb0bc670a9a1bafd37da3bf4afc095eab181
bogsi17 <28907925+bogsi17@users.noreply.github.com>
2024-05-02 16:56:51 +0100

    Fill introduction slide

--- src/main.js
+++ src/main.js
@@ -4,25 +4,25 @@ new App({
 	target: document.body,
 	props: {
 		// What's your name?
-		name: 'James Butler',
-		// In the following fiels you can either give a single string, 
+		name: 'Moritz Bogs',
+		// In the following fields you can either give a single string,
 		// or an array of bullet points
 		
 		// What do you associate with the term 'CI/CD'?
 		associations: [
-			'Ehm, what?'
+			'Automation, fast feedback, Fun building pipelines.'
 		],
 		// Which CI/CD tools do you use in your project?
-		tools: 'Jenkins, maybe?',
+		tools: 'Jenkins and Gitlab CI. Previously also Tekton+ArgoCD as well as AWS CodePipeline in an internal project.',
 		// What do you want to learn in this workshop? 
 		expectations: [
-			'what you are talking about'
+			'A lot of interesting questions :)'
 		],
 		// What do you like to do when you're not coding?
 		hobbies: [
 			'play chess', 
-			'do skydiving', 
-			'do my taxes'
+			'hiking',
+			'baking'
 		]
 	}
 });
